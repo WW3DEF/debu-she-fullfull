159624e1224b9ce9593dd6579746d39fde6f884c
HwiJae Lee <zzangka1122@gmail.com>
2022-03-30 17:21:44 +0900

    재밌게 배워봐요 호호~

--- 데부쉬풀풀/구조체.cpp
+++ 데부쉬풀풀/구조체.cpp
@@ -7,7 +7,11 @@ struct pos{
 int main()
 {
 	struct pos p;
+	struct pos *pp;
+	
+	pp=&p;
 	scanf("%d %d", &p.x, &p.x);
-	printf("%d %d", p.x, p.y);
+	printf("%d %d\n", p.x, p.y);
+	printf("%d %d\n", pp->x, pp->y);
 	return 0;
 }
